3cd622f56c33df973cc9e2dc80946d529b32a84e
Shri <shrinath.narayanan@gmail.com>
2024-03-13 11:11:30 -0700

    Fizzbuzz

--- 11_conditionals-and-loops/11_11_fizzbuzz.py
+++ 11_conditionals-and-loops/11_11_fizzbuzz.py
@@ -4,3 +4,17 @@
 # - If the number is divisible by 3, write `Fizz` instead of the number
 # - If the number is divisible by 5, write `Buzz` instead of the number
 # - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
+
+
+start = 1
+n = 100
+
+for num in range(1, n):
+    if num % 15 == 0:
+        print("Fizzbuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+        print(num)
